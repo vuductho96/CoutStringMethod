87b197a63e173269902acd3b649bbd2fd8af599e
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-09 21:07:10 +0700

    updated

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap chuoi: ");
+        string str = Console.ReadLine();
+
+        Console.Write("Nhap ky tu: ");
+        char = Console.ReadKey().KeyChar;// phương thức ReadKey nhận vào ký tự từ phàn phím .và xử ký đối tượng kiểu char
+        Console.WriteLine();
+
+        int count = 0;
+        foreach (char c in str)
+        {
+            if (c == ch)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("So lan xuat hien cua ky tu '{0}' trong chuoi la: {1}", ch, count);
+    }
+}
